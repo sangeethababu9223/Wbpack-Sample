f6cd8f531eac0870413dac962fb3f9add1155659
Sangeetha Babu <sangeetha.babu@eqs.com>
2022-05-24 22:41:28 +0530

    minify js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -37,4 +38,7 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new TerserPlugin()
+    ]
 }
